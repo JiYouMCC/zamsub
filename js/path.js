ba765fb389b0ee33ae87d1d2dd005f320cdb90b4
Ji You <nightghostjiyou@gmail.com>
2019-02-28 07:55:46 +0800

    close #70 close #72 修正一波在生成地图时发现的错误 (#69)
    * 修正数据错误
    
    * 修正ij枢纽坐标错误
    
    * 修复比利线显示错误
    
    * 修正SCP驻赞服总办事处名称错误
    
    * 改截图

--- js/path.js
+++ js/path.js
@@ -183,14 +183,15 @@ function getResult() {
     }
     var result = Dijkstra(graph, start);;
     var length = result[0][end];
+    var path = ToArray(result[1], end);
+    document.getElementById("result_svg").innerText = "";
     if (length == Number.MAX_VALUE) {
-        document.getElementById("result_path").innerText = "步行，飞行或游泳";
+        document.getElementById("result_svg").innerText = "步行，飞行或游泳";
         document.getElementById("result_length").innerText = "反正坐地铁到不了，你可以跳车试一试"
         return;
     }
-    var path = ToArray(result[1], end);
+    
     document.getElementById("result_length").innerText = length.toString() + "(约" + ConvertTime (length) + ")";
-    document.getElementById("result_svg").innerText = "";
     RanderSVG(path, lines, stations, "result_svg");
 }
 
