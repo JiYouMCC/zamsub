78aab614ab5638ea4423dcd6c98482d47409e09a
Jill Ye <7357434+JiYouMCC@users.noreply.github.com>
2022-03-02 14:06:45 +0800

    修改地图缩放比率

--- js/map.js
+++ js/map.js
@@ -5,6 +5,7 @@ var x_min = Number.MAX_VALUE;
 var x_max = -Number.MAX_VALUE;
 var z_min = Number.MAX_VALUE;
 var z_max = -Number.MAX_VALUE;
+var zoomRatio = 15;
 for (i in stations) {
     var centerpoint = stations[i].centerpoint();
     var x = centerpoint.x;
@@ -23,8 +24,8 @@ for (i in stations) {
         z_max = z;
     }
 }
-var width = 10 + (x_max - x_min) / 10 + 10;
-var height = 10 + (z_max - z_min) / 10 + 10;
+var width = 10 + (x_max - x_min) / zoomRatio + 10;
+var height = 10 + (z_max - z_min) / zoomRatio + 10;
 
 var svg = d3.select("#map")
     .append("svg")
@@ -43,12 +44,12 @@ var circle = gPoint.selectAll("circle")
     .enter()
     .append("circle");
 circle.attr("cx", function(station) {
-        return 10 - x_min / 10 + station.location[0].x / 10
+        return 10 - x_min / zoomRatio + station.location[0].x / zoomRatio
     })
     .attr("cy", function(station) {
-        return 10 - z_min / 10 + station.location[0].z / 10
+        return 10 - z_min / zoomRatio + station.location[0].z / zoomRatio
     })
-    .attr("r", 5)
+    .attr("r", 50/zoomRatio)
     .attr("stroke", "black")
     .attr("fill", "white")
     .attr("stroke-width", "2")
@@ -76,28 +77,28 @@ var path_g = gPath.selectAll("polyline")
 
 path_g.attr("points", function(p) {
         if (p.length == 2) {
-            var x1 = 10 - x_min / 10 + p[0].location[0].x / 10;
-            var y1 = 10 - z_min / 10 + p[0].location[0].z / 10;
-            var x2 = 10 - x_min / 10 + p[1].location[0].x / 10;
-            var y2 = 10 - z_min / 10 + p[1].location[0].z / 10;
+            var x1 = 10 - x_min / zoomRatio + p[0].location[0].x / zoomRatio;
+            var y1 = 10 - z_min / zoomRatio + p[0].location[0].z / zoomRatio;
+            var x2 = 10 - x_min / zoomRatio + p[1].location[0].x / zoomRatio;
+            var y2 = 10 - z_min / zoomRatio + p[1].location[0].z / zoomRatio;
             return x1 + "," + y1 + " " + x2 + "," + y2;
         } else {
             var result = "";
             for (var i = 0; i < p[2].length; i++) {
-                var x = 10 - x_min / 10 + p[2][i].x / 10;
-                var y = 10 - z_min / 10 + p[2][i].z / 10;
+                var x = 10 - x_min / zoomRatio + p[2][i].x / zoomRatio;
+                var y = 10 - z_min / zoomRatio + p[2][i].z / zoomRatio;
                 result += x + "," + y + " ";
             }
             return result;
         }
     })
     .attr("fill", "none")
-    .attr("stroke-width", 5)
+    .attr("stroke-width", 50 / zoomRatio)
     .attr("stroke-linecap", "round")
     .attr("stroke-dasharray", function(p) {
         var line = FindLine(p[0], p[1], edges);
         if (line.selfemployed) {
-            return "10,10";
+            return 100 / zoomRatio + "," + 100 / zoomRatio;
         }
         return ""
     })
